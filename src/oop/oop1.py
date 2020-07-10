b1286fbdbbec71f0a826602c738a9e83dddc585a
Katrina Hernandez <abqkatrina@gmail.com>
2020-07-10 09:11:52 -0600

    oop1 done, test passed

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -16,4 +16,28 @@
 # class Whatever:
 #     pass
 #
-# Put a comment noting which class is the base class
+# Put a comment noting which class is the base class:
+
+#vehicle defines the basic requirements of all kinds of vehicles
+class Vehicle:
+    pass
+
+# ground vehicles are vehicles, inherits attributes
+class GroundVehicle(Vehicle):
+    pass
+#cars and motorcycles are types of ground vehicles, and therefore vehicles
+class Car(GroundVehicle):
+    pass
+
+class Motorcycle(GroundVehicle):
+    pass
+
+#flight vehicles are vehicles, inherits attributes
+class FlightVehicle(Vehicle):
+    pass
+#starships and airplanes are types of flight vehicles, and therefore vehicles
+class Starship(FlightVehicle):
+    pass
+
+class Airplane(FlightVehicle):
+    pass
